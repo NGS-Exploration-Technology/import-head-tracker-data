0d47351633b720c9a7efafd4de9c87636279d7a9
eberkenp <eberkenp@ngs.org>
2018-02-27 17:20:35 -0500

    Revised head tracker conversion

--- convert_head_tracker.m
+++ convert_head_tracker.m
@@ -1,52 +1,71 @@
-TABLE = csvimport('SD_0226_172246.csv');
+TABLE = csvimport('SD_0227_143901.csv');
 table_size = size(TABLE);
-TABLE_DATA = cell2mat(TABLE(2:table_size(1),1:25));
+TABLE_DATA = cell2mat(TABLE(2:table_size(1),1:9));
 T_table = TABLE_DATA(:,1); %[s]
-%Roll_data = TABLE_DATA(:,5); %[rad]
-Roll_data = TABLE_DATA(:,6); %[rad]
-%Pitch_data = TABLE_DATA(:,6); %[rad]
-Pitch_data = TABLE_DATA(:,5); %[rad]
-Yaw_data = TABLE_DATA(:,7); %[rad]
+Q1 = TABLE_DATA(:,2); %[rad]
+Q2 = TABLE_DATA(:,3); %[rad]
+Q3 = TABLE_DATA(:,4); %[rad]
+Q4 = TABLE_DATA(:,5); %[rad]
 
-t_out = zeros(size(Roll_data));
-psi_out = zeros(size(Roll_data));
-theta_out = zeros(size(Roll_data));
-phi_out = zeros(size(Roll_data));
-Fx_out = zeros(size(Roll_data));
-Fy_out = zeros(size(Roll_data));
-Depth_out = zeros(size(Roll_data));
+t_out = zeros(size(Q1));
+phi_out = zeros(size(Q1));
+phi_offset = 0;
+theta_out = zeros(size(Q1));
+theta_offset = 0;
+psi_out = zeros(size(Q1));
+psi_offset = 0;
+Fx_out = zeros(size(Q1));
+Fy_out = zeros(size(Q1));
+Depth_out = zeros(size(Q1));
+
+ea_deg_old = zeros(1,3);
 
 dt = 0.01;
 
 index = 1;
-while(index<length(Roll_data))
-   theta = Pitch_data(index)*(180/pi);
-   phi = Roll_data(index)*(180/pi);
-   psi = Yaw_data(index)*(180/pi);
-   ea = SpinCalc('EA213toEA321',[phi,theta,psi]);
+while(index<length(Q1))
+   q = [Q1, Q2, Q3, Q4];
+   ea = quatern2euler(q(index,:));
    
-   %psi_out(index) = ea(1)*(180/pi);
-   if (ea(1)>180)
-       ea(1) = ea(1)-360;
+   ea_deg = ea.*(180/pi); %convert to degrees
+   
+   if (abs(ea_deg(1))>100)
+       if ((sign(ea_deg(1))==1)&&(sign(ea_deg_old(1))==-1))
+           phi_offset = phi_offset - 360;
+       end
+       if ((sign(ea_deg(1))==-1)&&(sign(ea_deg_old(1))==1))
+           phi_offset = phi_offset + 360;
+       end
    end
-   psi_out(index) = ea(1);
-   %theta_out(index) = ea(2)*(180/pi);
-   if (ea(2)>180)
-       ea(2) = ea(2)-360;
+   phi_out(index) = ea_deg(1) + phi_offset;
+   
+   if (abs(ea_deg(2))>100)
+       if ((sign(ea_deg(2))==1)&&(sign(ea_deg_old(2))==-1))
+           theta_offset = theta_offset - 360;
+       end
+       if ((sign(ea_deg(2))==-1)&&(sign(ea_deg_old(2))==1))
+           theta_offset = theta_offset + 360;
+       end
    end
-   theta_out(index) = ea(2);
-   %phi_out(index) = ea(3)*(180/pi);
-   if (ea(3)>180)
-       ea(3) = ea(3)-360;
+   theta_out(index) = ea_deg(2) + theta_offset;
+   
+   if (abs(ea_deg(3))>100)
+       if ((sign(ea_deg(3))==1)&&(sign(ea_deg_old(3))==-1))
+           psi_offset = psi_offset - 360;
+       end
+       if ((sign(ea_deg(3))==-1)&&(sign(ea_deg_old(3))==1))
+           psi_offset = psi_offset + 360;
+       end
    end
-   phi_out(index) = ea(3);
+   psi_out(index) = ea_deg(3) + psi_offset;
+   ea_deg_old = ea_deg;
    index = index + 1;
    t_out(index) = t_out(index-1) + dt; 
 end
 
 output_table = [t_out, Fx_out, Fy_out, Depth_out, psi_out, theta_out, phi_out];
 
-filename = ['control_program.csv'];
+filename = 'control_program.csv';
 headers = {'Time[s]', 'Fx[N]', 'Fy[N]', 'Depth[m]', 'Phi[deg]', 'Theta[deg]',' Psi[deg]'};
 csvwrite_with_headers(filename, output_table, headers);
 
